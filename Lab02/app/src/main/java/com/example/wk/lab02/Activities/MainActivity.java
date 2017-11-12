406cc636a09fe9e588724ada8fcc200122a30459
q w <wiciok1@gmail.com>
2017-11-12 11:33:11 +0100

    lab02 finished

--- Lab02/app/src/main/java/com/example/wk/lab02/Activities/MainActivity.java
+++ Lab02/app/src/main/java/com/example/wk/lab02/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wk.lab02.Activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -12,57 +13,47 @@ import com.example.wk.lab02.Fragments.SecondFragment;
 import com.example.wk.lab02.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonFragment1;
-    private Button buttonFragment2;
 
     private FirstFragment firstFragment;
     private SecondFragment secondFragment;
 
+    private final int SMSPermissionRequestCode = 1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //necessary for Android 6.0 and above:
+        requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, SMSPermissionRequestCode);
+
         setContentView(R.layout.activity_main);
         InitButtons();
 
         if (findViewById(R.id.fragment_container) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null) {
                 return;
             }
 
-            // Create a new Fragment to be placed in the activity layout
             firstFragment = new FirstFragment();
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
-
-            // Create a new Fragment to be placed in the activity layout
             secondFragment = new SecondFragment();
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            secondFragment.setArguments(getIntent().getExtras());
 
             // Add the fragment to the 'fragment_container' FrameLayout
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
-
         }
     }
 
     private void InitButtons()
     {
-        buttonFragment1 = (Button)findViewById(R.id.button_fragment1);
+        Button buttonFragment1 = (Button) findViewById(R.id.button_fragment1);
         buttonFragment1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeFragmentToFirst();
             }
         });
-        buttonFragment2 = (Button)findViewById(R.id.button_fragment2);
+        Button buttonFragment2 = (Button) findViewById(R.id.button_fragment2);
         buttonFragment2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ Lab02/app/src/main/java/com/example/wk/lab02/SmsReceiver.java
@@ -0,0 +1,15 @@
+package com.example.wk.lab02;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED"))
+            Toast.makeText(context, "SMS Received", Toast.LENGTH_SHORT).show();
+    }
+}
